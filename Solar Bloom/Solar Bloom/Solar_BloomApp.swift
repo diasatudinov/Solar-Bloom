1004bccfa8816688eccb4fa33ce17742a082d489
Dias <dias.atudinov@gmail.com>
2025-11-10 22:38:05 +0500

    app setup

---
+++ Solar Bloom/Solar Bloom/Solar_BloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_BloomApp.swift
+//  Solar Bloom
+//
+//  Created by Dias Atudinov on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Solar_BloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
