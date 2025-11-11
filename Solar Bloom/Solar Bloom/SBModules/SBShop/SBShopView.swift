e061451d36b1387c356e2fb3fc9aceac0db69e9d
Dias <dias.atudinov@gmail.com>
2025-11-11 22:55:52 +0500

    new files

---
+++ Solar Bloom/Solar Bloom/SBModules/SBShop/SBShopView.swift
@@ -0,0 +1,18 @@
+//
+//  SBShopView.swift
+//  Solar Bloom
+//
+//  Created by Dias Atudinov on 11.11.2025.
+//
+
+import SwiftUI
+
+struct SBShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SBShopView()
+}
